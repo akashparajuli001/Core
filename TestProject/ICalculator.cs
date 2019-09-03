b45aa56603163de2209d5ccd0ffb5a6370d91aa4
akash parajuli <akash.parajuli001@gmail.com>
2019-09-03 17:28:10 -0500

    test

---
+++ TestProject/ICalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    public interface ICalculator
+    {
+        int Add(int num1, int num2);
+        int Mul(int num1, int num2);
+    }
+}
